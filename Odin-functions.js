83afb19e202195c761e7cf762a294bf067d30726
DaryllCulas <culasdaryll99@gmail.com>
2023-05-08 15:51:12 +0800

    initializing document objects inside the function at Odin-functions.js

---
+++ Odin-functions.js
@@ -0,0 +1,7 @@
+function calculateFunction() {
+
+const CalculateSpanElement = document.querySelector('.Answer-Span');
+const FirstIntegerElement = document.querySelector('.FirstInteger');
+const SecondIntegerElement = document.querySelector('.SecondInteger');
+
+}
