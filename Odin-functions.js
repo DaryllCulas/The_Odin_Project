59559dcc73ea6edebc5cee616636d17c18b0bad6
DaryllCulas <culasdaryll99@gmail.com>
2023-05-08 16:39:36 +0800

    Calculate Function parametized is done

--- Odin-functions.js
+++ Odin-functions.js
@@ -1,7 +1,19 @@
-function calculateFunction() {
+function Calculate(FirstIntegerElement, SecondIntegerElement) {
 
 const CalculateSpanElement = document.querySelector('.Answer-Span');
-const FirstIntegerElement = document.querySelector('.FirstInteger');
-const SecondIntegerElement = document.querySelector('.SecondInteger');
+const first = parseInt(FirstIntegerElement.value);
+const second = parseInt(SecondIntegerElement.value);
+const result = first + second;
+
+CalculateSpanElement.innerHTML = result;
+
+function Reset() {
+  if(first.trim()!=="" || second.trim()!=="") {
+    FirstIntegerElement.value = "";
+    SecondIntegerElement.value = "";
+  }
+}
+
 
 }
+
